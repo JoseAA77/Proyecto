b46f0f6484557a7f6561a0fba4ff6dad79959c15
CeDeC-04 <suprasteria@gmail.com>
2024-11-20 14:41:33 +0100

    Update classe_Buzzer_Passiu_underconstruction.py

--- Llibreria/classe_Buzzer_Passiu_underconstruction.py
+++ Llibreria/classe_Buzzer_Passiu_underconstruction.py
@@ -25,3 +25,40 @@ class Buzzer:
     def cleanup(self):
         """Neteja la configuració de GPIO."""
         GPIO.cleanup()
+
+
+
+
+
+
+
+
+
+#####################
+
+        #### Exemple amb un Buzzer Passiu
+ Un buzzer passiu necessita una freqüència específica. Amb aquest exemple es generen diferents
+ sons.
+    # Importa els mòduls necessaris
+ import RPi.GPIO as GPIO
+ import time
+ GPIO.setmode(GPIO.BCM)
+ BUZZER_PIN = 17
+ GPIO.setup(BUZZER_PIN, GPIO.OUT)
+    # Defineix una freqüència (en Hz) i la durada (en segons)
+ pwm = GPIO.PWM(BUZZER_PIN, 440)  # Freqüència inicial de 440Hz
+ pwm.start(50)  # Duty cycle al 50%
+ try:
+    pwm.ChangeFrequency(523)  # Nota Do
+    time.sleep(0.5)
+    pwm.ChangeFrequency(587)  # Nota Re
+    time.sleep(0.5)
+    pwm.ChangeFrequency(659)  # Nota Mi
+    time.sleep(0.5)
+    pwm.ChangeFrequency(698)  # Nota Fa
+    time.sleep(0.5)
+    pwm.stop()
+ finally:
+    GPIO.cleanup()
+ ### Explicació- **GPIO.output**: s'usa per controlar el buzzer actiu amb un senyal de corrent contínua.- **GPIO.PWM**: permet generar senyals d'ona quadrada amb diferents freqüències, ideal per a
+ buzzers passius.
