13a0f4692ca8c9afb37250d6163ac95a6a3a9d1d
JoseAA77 <joseaa77@hotmail.com>
2024-11-25 22:37:25 +0100

    Librerias I2C Display
    Librerias necesarias para poder visualizar datos en el display LCD 4x20

---
+++ Llibreria/Exemples/clock_LCD.py
@@ -0,0 +1,26 @@
+from lcd_display import lcd
+from subprocess import * 
+from time import sleep, strftime
+from datetime import datetime
+
+lcd = lcd()
+
+cmd = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+temp_cpu = '/usr/bin/vcgencmd measure_temp' # mide la temperatura de la CPU
+
+count = 0
+
+def run_cmd(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
+
+while 1:
+    lcd.clear()
+    ipaddr = run_cmd(cmd)
+    temp = run_cmd(temp_cpu)
+
+    lcd.display_string(datetime.now().strftime('%b %d  %H:%M:%S'),1)
+    lcd.display_string('IP %s' % ( ipaddr ),2 )
+    lcd.display_string('%s' % ( temp ),3 )
+    sleep(2)
