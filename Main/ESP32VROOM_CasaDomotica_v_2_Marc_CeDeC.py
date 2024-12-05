4d986c35b8dfdd3f375d2a40f9fd5e3c7792cf08
CeDeC-04 <suprasteria@gmail.com>
2024-12-05 13:51:17 +0100

    merging

--- Main/ESP32VROOM_CasaDomotica_v_2_Marc_CeDeC.py
+++ Main/ESP32VROOM_CasaDomotica_v_2_Marc_CeDeC.py
@@ -7,69 +7,45 @@
 #acció cap a un actuador, aquesta variable estarà plena amb el canvi a realitzar i actuarà en consequwencia el sistema, per exemple amb una
 #referència del q s'està modificant.
 
-############################       BACKLOG      #################################
-#En una primera versió, enviarem només el diccionari dinàmic, la part del diccionari estatic la posarem en un diccionari intern de cada placa.
-#Ademés, hi hauran estats dels diccionaris que no s'enviaran a la raspberry i s'haurà de fer en una segona versió
+#estat_casa = { llum_menjador : {llums,5}, llum_cuina : {llums,0}, sensor_gas : {gas,0}, estat_alarma : {alarma,{"armada","desactivada"}}}
+#estat_casa = { llum : {menjador : 5}, llum : {cuina : 0}, gas : {mq135 : 0}, {alarma : {alarma : {"armada","desactivada"}}}
+#keys_estat_casa = { llum : "llum", gas : "gas", alarma : "alarma"}
+#estat_casa = { llum_menjador : 5, llum_cuina : 0, gas_cuina : 0, alarma_general : {"armada","desactivada"}}
 
-#import machine
-#from machine import Pin, I2C
-from machine import *
-import time
-from classe_Buzzer_Passiu import *
-#from classe_Buzzer_Passiu import Buzzer_Passiu
-from classe_MQ2_esp32 import *
-#from classe_MQ2 import MQ2
-#import re
-from classe_WIFI_ESP32WROOM import *
-#from classe_WIFI_RaspPI3 import *
-from classe_pca9685_esp32 import *
-from classe_pulsador_esp32 import *
-#from classe_pca9685_esp32 import PCA9685
-#from classe_pulsador_esp32 import Pulsador
+#objecte_casa = { llum_menjador : pcaLM, llum_cuina : pcaLC, gas_cuina : mq1351, alarma_general : alarmaG}
+#estat_casa = { llum_menjador : "5T" '''(aixó seria valor 5% i True, és a dir encés. Si fos F, seria apagat (False))''', llum_cuina : 0, gas_cuina : 0, alarma_general : {"armada","desactivada"}}
+
+#estat_casa = { "llum_menjador" : {"estat" : 5, "objecte" : llum_M}, "alarmaGeneral": {"estat": "activada", "armada" : True, "objecte": "alarmaGeneral"},
+
+#En una primera versió, enviarem només el diccionari dinàmic, la part del diccionari estatic la posarem en un diccionari intern de cada placa.
+
+import re
+from machine import Pin, I2C
+from classe_WIFI_RaspPI3 import *
 import ujson  # per a MicroPython
 #import json # per a Python
+from classe_pca9685_esp32 import PCA9685
+from classe_pulsador_esp32 import Pulsador
 
-
-# PINs per a sensors i actuadors
-    #faltaria passar-ho al chip multiplexor
-SENSOR_MAG_ventanas = 17 
-    #faltaria passar-ho al chip multiplexor
-SENSOR_MAG_puerta = 27 
-SENSORES_IR_PINes = [22, 23, 24, 25]
-BUZZER_PIN = 18
-SENSOR_MQ2_PIN = 4
-
+missatge_enviar = '{}'
 
 # Creacio objectes
 COMs = WIFI()
-#Alarma
-buzzer = Buzzer_Passiu(BUZZER_PIN)
-sensor_gas = MQ2(SENSOR_MQ2_PIN, platform="pi_pico")
-    #faltarua passar-ho al chip multiplexor
-sensor_mag_serie = machine.Pin(SENSOR_MAG_ventanas, machine.Pin.IN, machine.Pin.PULL_UP) 
-    #faltarua passar-ho al chip multiplexor
-sensor_mag_indep = machine.Pin(SENSOR_MAG_puerta, machine.Pin.IN, machine.Pin.PULL_UP) 
-sensores_ir = [machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_UP) for pin in SENSORES_IR_PINes]
-#Polsadors i llums
-falta definir polsadors d'alarma, timbre...
 i2c = I2C(0, scl=Pin(22), sda=Pin(21))
 pulsador = [Pulsador(13, False, "pi_pico"), Pulsador(26, False, "pi_pico"), Pulsador(27, False, "pi_pico"),
             Pulsador(12, False, "pi_pico"), Pulsador(14, False, "pi_pico"), Pulsador(25, False, "pi_pico"),
             Pulsador(33, False, "pi_pico")]
 
 
-# Variables, declaració
-missatge_enviar = '{}'
-primerarmat = True
-primerDESarmat = False
 
 while True:
     
     try:
         # Realitza comunicacio
-        missatge_rebut = COMs.WIFI_Comunicacio('{}')
+        missatge_rebut = COMs.WIFI_Comunicacio(missatge_enviar)
         missatge_enviar = '{}' #en la primera versió, es suposa que a cada tecla s'envia el missatge. pero es possible que en altres versions s'enviin ´es canvis en un enviament i en fils
         
+
         # Actualitza diccionari si es rebut algun canvi
         if missatge_rebut != '':
             diccionari_rebut = to_diccionari(missatge_rebut)
@@ -79,9 +55,9 @@ while True:
                 estat_objectes_casa[key] = diccionari_rebut[key]
                 
                 ########
-                fer que ejecute los cambios
+                es que ejecute los cambios
 
-
+        
 #########        
         # Diccionari anterior
         diccionari_estats_anterior = sorted(estat_objectes_casa.items()) # Ordena segons les claus    
@@ -93,31 +69,44 @@ while True:
         capta els valors dels sensors #fer les coses seguint els sensors
         el diccionari s'actualitza a cada sensor pel que passa
         així mateix la variable string del diccionari a enviar a la raspberry també s'actualitza
-        # Verificación de estado de la alarma
 
         
 #########        
         if diccionari ara != a abans, entra i canvia coses. Si és el mateix significa que no ha passat res i nno cal q faci res
         llegeix el diccionari i actualitza l'estat dels actuadors (llums, buzzer, servo...) en funció del nou valor del diccionari que ha aportat el sensor corresponent
         #el diccionari s'haur``a actualitzat despres de pasar per codi
+'''
+keys = ["llumcuina", "llumpasadis"]                
+for key in keys:
+    valor_objectes[key] = True
+
+ "Llum_Cuina" : 0,
+'''
+
+def detectar_pulsadors():
+    keys_pols = ["Pols_Llum_Cuina", "Pols_Llum_Passadis", "Pols_Llum_Menjador", "Pols_Llum_Lavabo",
+            "Pols_Llum_Habitació_1", "Pols_Llum_Habitació_2", "Pols_Llum_Habitació_3"]
+    keys_llum = ["Llum_Cuina", "Llum_Passadis", "Llum_Menjador", "Llum_Lavabo",
+            "Llum_Habitació_1", "Llum_Habitació_2", "Llum_Habitació_3"]
+
+    for key in range(len(keys_pols)):
+        accion = objectes_casa[keys_pols[key]].gestionar_pulsacions(1)
         
-        if estat_objectes_casa["Alarma_Intrusio_Total"][0] == "T": #l'armat Total te prioritat        
-            alarma_2("Total")
-        elif estat_objectes_casa["Alarma_Intrusio_Perimetral"][0] == "T": #si no esta total comprova si esta perimetral
-            alarma_2("Perimetral")
+        if accion[0] == True:
+            pca.alterna(objectes_casa[keys_llum[key]])
+            pca.bajando = True
+        elif accion[1] == True:
+            pca.bajar_subir(objectes_casa[keys_llum[key]])
+        
+        if accion[0] or accion[1]:
+            estat_objectes_casa[keys_llum[key]] == True
         else:
-            alarma_2("Total") #podria ser alarma_2("Perimetral"), tant ne fa pq estan desarmades les dues
+            estat_objectes_casa[keys_llum[key]] == False
+
+
 ##        
 fer una funció per objecte o si pot ser per grup d'objectes que es cridara en l'apartat 'BBBB
   ##      
-  
-  
-  
-    except Exception as e:
-        print("Error:", e)
-    except KeyboardInterrupt:
-        print("Apagando sistema...")
-        break
         # Canvi en el diccionari
         
  '''       # Crea el missatge a enviar, si no hi ha canvis, envia text buit
@@ -177,112 +166,6 @@ def to_diccionari(text): # Per a Python
     
     return diccionari
 '''           
-#### Alarma
-#### Alarma
-#### Alarma
-#### Alarma
-
-# Función para verificar sensores ALARMA INTRUSSIÓ
-def verificar_sensores(modo): # CeDeC: no entenc que fa
-    if modo == "Perimetral":
-        return sensor_mag_serie.value() == 0 or sensor_mag_indep.value() == 0
-    elif modo == "Total":
-        if verificar_sensores("Perimetral"):
-            return True
-        return any(sensor.value() == 0 for sensor in sensores_ir)
-    return False
-
-# Función para monitorear el sensor de gas
-def verificar_gas2():
-    if sensor_gas.detecta_particules():
-        estat_objectes_casa["Alarma_Gas"] = True
-        buzzer.melodia("alarma_gas")
-    else:
-        estat_objectes_casa["Alarma_Gas"] = False
-        buzzer.desactiva()
-
-def verificar_gas():
-    if sensor_gas.detecta_particules():
-        print("¡Gas detectado!")
-        estat_objectes_casa["Alarma_Gas"] = True
-        buzzer.melodia("alarma_gas")
-
-#primerarmat = True
-#primerDESarmat = False
-
-# Función principal de la alarma
-def alarma_2(modo):
-    verificar_gas2()
-    if estat_objectes_casa[f"Alarma_Intrusio_{modo}"][0] == "T":
-        if primerarmat:
-            armar_alarma(modo)
-            primerarmat,primerDESarmat = False, True
-        if estat_objectes_casa[f"Alarma_Intrusio_{modo}"][1] == "T":
-            if verificar_sensores(modo):
-                buzzer.melodia("alarma_intrusio")
-#            else:
-#                buzzer.desactiva()
-    elif estat_objectes_casa[f"Alarma_Intrusio_{modo}"][0] == "F":
-        if primerDESarmat:
-            desarmar_alarma2(modo)
-            primerarmat,primerDESarmat = True, False
-
-
-def alarma(modo):
-    print(f"Alarma activada en modo: {modo.upper()}")
-    buzzer.melodia("to_armat")
-    estat_objectes_casa[f"Alarma_Intrusio_{modo.capitalize()}"] = "TT"  # Armat y Activo
-    try:
-        while estat_objectes_casa[f"Alarma_Intrusio_{modo.capitalize()}"] == "TT":
-            verificar_gas()
-            if verificar_sensores(modo):
-                print("¡Alarma activada!")
-                buzzer.melodia("alarma_intrusio")
-                time.sleep(2)
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        desarmar_alarma(modo)
-
-def armar_alarma(modo):
-    estat_objectes_casa[f"Alarma_Intrusio_{modo.capitalize()}"][0] = "T"  # Armat
-    buzzer.melodia("to_armat")
-
-def desarmar_alarma2(modo):
-    estat_objectes_casa[f"Alarma_Intrusio_{modo.capitalize()}"][0] = "F"  # Desarmat
-    buzzer.desactiva()
-    time.sleep(0.5)
-    buzzer.melodia("to_desarmat")
-    
-def desarmar_alarma(modo):
-    print(f"Alarma desactivada: {modo.upper()}")
-    estat_objectes_casa[f"Alarma_Intrusio_{modo.capitalize()}"] = "FF"  # Desarmat y No Activo
-    buzzer.melodia("to_desarmat")
-
-
-### Polsadors
-### Polsadors
-### Polsadors
-### Polsadors
-
-def detectar_pulsadors():
-    keys_pols = ["Pols_Llum_Cuina", "Pols_Llum_Passadis", "Pols_Llum_Menjador", "Pols_Llum_Lavabo",
-            "Pols_Llum_Habitació_1", "Pols_Llum_Habitació_2", "Pols_Llum_Habitació_3"]
-    keys_llum = ["Llum_Cuina", "Llum_Passadis", "Llum_Menjador", "Llum_Lavabo",
-            "Llum_Habitació_1", "Llum_Habitació_2", "Llum_Habitació_3"]
-
-    for key in range(len(keys_pols)):
-        accion = objectes_casa[keys_pols[key]].gestionar_pulsacions(1)
-        
-        if accion[0] == True:
-            pca.alterna(objectes_casa[keys_llum[key]])
-            pca.bajando = True
-        elif accion[1] == True:
-            pca.bajar_subir(objectes_casa[keys_llum[key]])
-        
-        if accion[0] or accion[1]:
-            estat_objectes_casa[keys_llum[key]] == True
-        else:
-            estat_objectes_casa[keys_llum[key]] == False
 
 
 # diccionaris estàtics
@@ -327,9 +210,9 @@ objectes_casa = {
                 "Servo_PEntrada" : Servo_PEntrada,
                 
                 #"Alarma_Gas" : Alarma_Gas, -> 1
-                "Alarma_Gas" : sensor_gas,
-                "Alarma_Intrusió_Perimetral" : alarma_2("Perimetral"), # Ha de contenir els sensors reed
-                "Alarma_Intrusió_Total" : alarma_2("Total") # Ha de contenir els sensors de proximitat i els reed
+                "Alarma_Gas" : Gas_MQ135,
+                "Alarma_Intrusió_Perimetral" : Alarma_Intrusió_Perimetral, # Ha de contenir els sensors reed
+                "Alarma_Intrusió_Total" : Alarma_Intrusió_Total # Ha de contenir els sensors de proximitat i els reed
                 }
 
 # diccionaris dinàmics
